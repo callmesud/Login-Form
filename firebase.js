18e7b20b9e19bd090ea34ec568b8369711d091d3
callmesud <91628939+callmesud@users.noreply.github.com>
2022-02-07 11:37:39 +0530

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBD7X7QrOB3Y1fTwfYqGgJlG1v-jMNPjP0",
+    authDomain: "eduontech-1217a.firebaseapp.com",
+    projectId: "eduontech-1217a",
+    storageBucket: "eduontech-1217a.appspot.com",
+    messagingSenderId: "51118166774",
+    appId: "1:51118166774:web:f02d3d5bffa7ac546d92d7",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+//firebase.analytics();
